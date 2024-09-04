4d14eafe56179584403e688bd1dc69444a93712d
RyuMinSoo <sonicus94.dev@gmail.com>
2024-09-04 11:24:59 +0900

    complete challenge5

--- pages/challenge5.py
+++ pages/challenge5.py
@@ -1,116 +1,128 @@
 import streamlit as st
-
-
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import CacheBackedEmbeddings
-from langchain_openai import OpenAIEmbeddings
-from langchain.storage import LocalFileStore
-from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.storage import LocalFileStore
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.callbacks.base import BaseCallbackHandler
 
 
 
-def embedding_file(file):
+
+#---------- function
+def format_docs(docs):
+    return '\n\n'.join(doc.page_content for doc in docs)
+
+@st.cache_resource(show_spinner="embedding..")
+def embed_file(file, api_key):
     file_name = file.name
+    file_path = f'././.cache/files/{file_name}'
     file_content = file.read()
-    file_path = f"././.cache/files/{file_name}"
-    with open(file_path, 'wb') as f: 
-        f.write(file_content)
+    with open(file_path, "wb") as f:
+        f.write(file_content)    
 
-    #document_load
+    #load_file    
     loader = TextLoader(file_path)
 
-    #split
+    #split file
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
     docs = loader.load_and_split(text_splitter=splitter)
-    
-    #embedding
-    embeddings = OpenAIEmbeddings()
-    cache_dir = LocalFileStore(f"././.cache/embeddings/{file_name}")
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
-    vectorstores = Chroma.from_documents(docs, cached_embeddings)
-    retriever = vectorstores.as_retriever()
+    print(len(docs))
+
+    #embeddings & cache
+    embeddings = OpenAIEmbeddings(api_key=api_key)    
+    cache_path = LocalFileStore(f"././.cache/embeddings/{file_name}") 
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_path)
+
+    #vectorstore > retriever
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    print("retriever 생성 완료")
     return retriever
 
-
-def format_docs(docs):
-    return '\n\n'.join(doc.page_content for doc in docs)
-
-
-def send_message(role, message):
+def send_message(role, message, save=True):
     with st.chat_message(role):
-        st.write(message)
+        st.markdown(message)
+    if save:
+        save_message(role, message)
 
+def paint_history():
+    for message in st.session_state['messages']:
+        send_message(
+            message["role"],
+            message["message"],
+            save=False,
+        )
 
-#---------- header & config
-st.set_page_config(
-    page_title="Document Upload Chatbot"
-)
+def save_message(role, message):
+    st.session_state["messages"].append({"message": message, "role": role})
 
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
 
-st.title('Chatbot for Document')
-st.markdown(
-    '''
-    Upload your document to the sidebar and ask questions.
-    
-    We'll find information and answer your questions in the documentation.
-'''
-)
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
 
+    def on_llm_end(self, *args, **kwargs):
+        save_message("ai", self.message)
 
-#---------- variables
-llm = ChatOpenAI(temperature=0.1)
-prompt = ChatPromptTemplate.from_messages([
-    (
-        'system',
-        '''
-        Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
-        Context: {context}
-        '''
-    ),
-    ('human', '{question}')
-])
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
 
 
 #---------- sidebar
 with st.sidebar:
+    api_key = st.text_input(label="Write your API Key")
     file = st.file_uploader(
         label="Upload File",
-        type=['txt']        
+        type=["txt"]
     )
     st.session_state
 
-
 #---------- main
 if file:
-    #준비
-    retriever = embedding_file(file)
-    with st.status(label='Loading Documents...') as status:        
-        status.update(label='Ask about Document!!')
-    send_message('ai', f'Hello, Ask a question about the {file.name}!!')    
-    
-    #채팅창 입력
-    message = st.chat_input(
-        placeholder="Ask anyting about your file"
-    )
-    if message:
-        #물어봄
-        send_message('human', message)    
-        
-        #찾아서 답주기
-        chain = {
-            'context': retriever | RunnableLambda(format_docs),
-            'question': RunnablePassthrough()
-        } | prompt | llm
-        
-        result = chain.invoke() #답추출
+    retriever = embed_file(file, api_key)
 
-        #답그리기
-        send_message('ai', result)
+    with st.status(label="Loading Document...") as status:
+        status.update(label="Embedding Document...")
+        status.update(label="Ask about Document!")
+    send_message('ai', f'Hello, Ask a question about the {file.name}!!', save=False)
+
+    paint_history()
+
+    message = st.chat_input(placeholder="Ask me about your document")
+    if message:
+        send_message('human', message, save=True) #질문 그리기 및 저장
+        llm = ChatOpenAI(
+            temperature=0.1,
+            api_key=api_key,
+            streaming=True,
+            callbacks=[ChatCallbackHandler()]
+        )
+        prompt = ChatPromptTemplate.from_messages([
+            (
+                'system',
+                """
+                    너는 도움을 주는 훌륭한 조수야. 주어지는 문서에서 묻는 말에 대한 답을 찾아줘. 만약 문서안에 답이 없다면 모르겠다고 대답해.\n\n{context}
+                """,
+            ),
+            ('human', '{question}')
+        ])
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough(),
+        } | prompt | llm
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+else:
+    st.session_state['messages'] = []
